1941d26861f70cfa6c11190f9c4f0c4f1a4fdeb7
daiyu <122267888@qq.com>
2015-09-16 10:35:02 +0800

    reverseinteger.java

---
+++ Java/7_ReverseInteger.java
@@ -0,0 +1,31 @@
+/*************************************************************************
+    > File Name: 7_ReverseInteger.java
+    > Author: daiyu
+	> url:https://leetcode.com/problems/reverse-integer/
+    > Created Time: 三  9/16 10:31:13 2015
+    注意溢出处理，此处对操作的result用long类型，防止溢出，最大最小值定义如integer.max_value
+    如果溢出返回0，shit oj，移位操作即％ long－> int 可以现将long转化为字符串，再用integer.parseint
+ ************************************************************************/
+public class Solution {
+    public int reverse(int x) {
+        long result = 0;
+        int max = Integer.MAX_VALUE;
+        int min = Integer.MIN_VALUE;
+        
+        if (x == 0) 
+            return x;
+        
+        while (x != 0) {
+            int tmp = x % 10;
+            result = result * 10 + tmp;
+            
+            if (result > max || result < min) {
+                return 0;
+            }
+                                    
+            x = x / 10;
+        }
+        
+        return Integer.parseInt(String.valueOf(result));        
+    }
+}
