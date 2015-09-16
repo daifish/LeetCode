63de1676eecb830a9c9bf0e396ab22eb5e910752
DaiYu <122267888@qq.com>
2015-09-16 09:30:22 +0800

    Update 5_Longest_Palindromic_Substring.java
    添加了解题思路和个人信息

--- Java/5_Longest_Palindromic_Substring.java
+++ Java/5_Longest_Palindromic_Substring.java
@@ -1,3 +1,9 @@
+/*
+遍历一遍字符串，对每位字符串进行同时向左向右的探测，判断是否符合回文规则，注意考虑奇偶数两种情况
+author:daiyu
+time:2015-9-16
+url:https://leetcode.com/problems/longest-palindromic-substring/
+*/
 public class Solution {
     public String longestPalindrome(String s) {
         if (s.isEmpty()) {
@@ -35,7 +41,13 @@ public class Solution {
     	return subS;
     }
 }
+/*
+思路：首先处理单字符的时候 即table[i][i]置为1，再处理步长为2的时候，即table[i][i + 1]，如果两者相等，则置为1
+同时取最长字符串，接下来则进行循环（步长从3到字符串的长度），工作指针i从0开始到s.length() - 步长， 右侧工作指针
+一直为i ＋ step - 1, 判断i j 处的值是否相等，如果相等，则table[i][j]= table[i + 1][j - 1](即里层的值) 如果当前的table[i][j]
+值为1，且此时的步长大于给定的maxlen,更新longeststr返回值和maxlen，如果i j处值不等则table[i][j]置0
 
+*/
 public class Solution2{
 	public String longestPalindromic(String s) {
 		int maxLen = 0;
@@ -51,12 +63,14 @@ public class Solution2{
 			
 		for (int i = 0; i <= length; i++) {
 			table[i][i] = 1;
+			maxLen = 1;
 		}
 		
 		for (int i = 0 ; i <= length - 2; i ++){
 			if (s.charAt(i) == s.charAt(i + 1)) {
 				table[i][i + 1] = 1;
 				longestStr = s.substring(i, i + 2);
+				maxLen = 2;
 			}
 		}
 		
