a8123d1bf2888ae13be08655bbf21dc57c2399d7
DaiYu <122267888@qq.com>
2015-09-26 17:14:53 +0800

    Update 002_AddTwoNumbers.java
    添加了解题思路

--- Java/002_AddTwoNumbers.java
+++ Java/002_AddTwoNumbers.java
@@ -8,6 +8,11 @@
  * author:daiyu
  * time :2015-9-15
  * url:https://leetcode.com/problems/add-two-numbers/
+ * 思路：定义一个工作链表，当l1,l2都不是最后一位时，进行加法处理（判断是否有进位，如果有，生成一个为1的下一位，l1,l2向下移动）
+ * 如果l1,l2都到最后时进行次进位处理
+ * 剩下的两种情况相同：如果l1到达末尾，如果没有进位，直接将l2接到工作链表后面
+ * 如果有进位，生成一个进位，将l2移到下一位，如果l2之后仍有节点，将l2的节点与进位想加进行溢出判断，跳出之后
+ * 如果是由于没有溢出跳出while，则将l2直接接到工作指针之后即可， 如果是由于到最后一位而跳出while，则进行次溢出判断
  */
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
