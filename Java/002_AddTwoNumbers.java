3be6eb0f7e70317d36613f8e1428fed87916fe89
DaiYu <122267888@qq.com>
2015-12-15 16:07:11 +0800

    Update 002_AddTwoNumbers.java

--- Java/002_AddTwoNumbers.java
+++ Java/002_AddTwoNumbers.java
@@ -115,3 +115,43 @@ public class Solution {
         return l3;
     }
 }
+//5ms
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ * 每次取l1,l2的一位加到sum里，当前工作链表的值为l1.val+l2.val+sum，sum进行/运算，如果l1或l2或进位有值，则生成一个进位
+ * 工作指针移动
+ */
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode(0);
+        ListNode cur = result;
+        int sum = 0;
+        
+        while(true) {
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            } 
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            cur.val = sum % 10;
+            sum /= 10;
+            
+            if (l1 !=null || l2 != null || sum != 0) {
+                cur.next = new ListNode(0);
+                cur = cur.next;
+            } else {
+                break;
+            }
+        }
+        
+        return result;
+    }
+}
