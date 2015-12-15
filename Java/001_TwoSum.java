33bb86ffbf1797d1efdf3676ca9c55595e9334fe
DaiYu <122267888@qq.com>
2015-12-15 15:56:26 +0800

    Update 001_TwoSum.java
    添加了新的 解体方法，使用hashmap，提高了代码效率， 先将所有数据都放入hashmap中，再判断是否有2个值满足 条件

--- Java/001_TwoSum.java
+++ Java/001_TwoSum.java
@@ -27,3 +27,27 @@ public Class Solution{
 		return result;
 	}
 }
+//7ms 
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        
+        int[] result = new int[2];
+        int length = nums.length;
+        
+        for(int i = 0; i < length; i++){
+            map.put(nums[i], i);
+        }
+        
+        for(int i = 0; i < length; i++){
+            int gap = target - nums[i];
+            if(map.get(gap) != null && map.get(gap) != i){
+                result[0] = i + 1;
+                result[1] = map.get(gap) + 1;
+                break;
+            }
+        }
+        
+        return result;
+    }
+}
