3e453f6c15a2c1dc9c5edb8b86e4111f29582308
DaiYu <122267888@qq.com>
2015-12-16 15:07:04 +0800

    Update 022_GenerateParentheses.java

--- Java/022_GenerateParentheses.java
+++ Java/022_GenerateParentheses.java
@@ -3,6 +3,7 @@
     > Author: daiyu
 	> Url: https://leetcode.com/problems/generate-parentheses/
     > Created Time: 五  9/18 10:43:03 2015
+    采用递归树的思想，当左括号数大于右括号数时可以加左或者右括号，否则只能加左括号，当左括号数达到n时，剩下全部
  ************************************************************************/
 public class Solution {
     public List<String> generateParenthesis(int n) {
@@ -30,4 +31,28 @@ public class Solution {
         rec(n, left, right + 1, str + ")", al);
         
     }
+}
+//
+public class Solution {  
+    public ArrayList<String> generateParenthesis(int n) {  
+        // Note: The Solution object is instantiated only once and is reused by each test case.  
+        ArrayList<String> res = new ArrayList<String>();  
+        generate(res, "", 0, 0, n);  
+        return res;  
+    }  
+    public void generate(ArrayList<String> res, String tmp, int lhs, int rhs, int n)  
+    {  
+        if(lhs == n)  
+        {  
+            for(int i = 0; i < n - rhs; i++)  
+            {  
+                tmp += ")";  
+            }  
+            res.add(tmp);  
+            return ;  
+        }  
+        generate(res, tmp + "(", lhs + 1, rhs, n);  
+        if(lhs > rhs)  
+            generate(res, tmp + ")", lhs, rhs + 1, n);  
+    }  
 }
