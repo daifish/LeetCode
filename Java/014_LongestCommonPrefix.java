71f62323d3af91c88f438fae99b2a084b556e218
DaiYu <122267888@qq.com>
2015-12-16 11:02:26 +0800

    Update 014_LongestCommonPrefix.java

--- Java/014_LongestCommonPrefix.java
+++ Java/014_LongestCommonPrefix.java
@@ -37,3 +37,21 @@ public class Solution {
         return result;
     }
 }
+//7ms
+public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+        
+        for (int i = 0; i < strs[0].length(); i ++) {
+            for (int j = 0; j < strs.length; j ++) {
+                if (strs[j].length() <= i || strs[j].charAt(i) != strs[0].charAt(i)) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        
+        return strs[0];
+    }
+}
