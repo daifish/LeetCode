d4c5fe613c63e9739f03c7e558ed5945b1085bdf
DaiYu <122267888@qq.com>
2015-12-16 10:47:20 +0800

    Update 011_ContainerWithMostWater.java

--- Java/011_ContainerWithMostWater.java
+++ Java/011_ContainerWithMostWater.java
@@ -45,3 +45,22 @@ public class Solution {
         return max;
     }
 }
+//8ms
+public class Solution {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int area = 0;
+        
+        while (left < right) {
+            area = Math.max(area, Math.min(height[left], height[right]) * (right - left));
+            if (height[left] > height[right]) {
+                right --;
+            } else {
+                left ++;
+            }
+        }
+        
+        return area;
+    }
+}
