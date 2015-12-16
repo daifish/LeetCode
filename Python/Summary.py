e11cbcc0930ce703a91c9e9ae6a249d8dee2a72a
DaiYu <122267888@qq.com>
2015-12-16 16:41:52 +0800

    Create Summary.py

---
+++ Python/Summary.py
@@ -0,0 +1,7 @@
+1.对于一个字符串s 可以使用enumerate(s)遍历s的index和相应的item
+用法：for i, ch in enumerate(s)
+2.cmp(x,y)->> #如果 x < y 返回 -1, 如果 x == y 返回 0, 如果 x > y 返回 1。
+所以用来判断正负号最好 如cmp(x,0)
+3.python中str函数isdigit、isdecimal、isnumeric的区别: http://www.tuicool.com/articles/vIVfqa
+4.nums数组排序nums.sort() 相应的java排序是Array.sort(nums)
+5.python并不支持i++这类语法
