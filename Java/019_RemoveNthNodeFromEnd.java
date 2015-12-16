bd0e3008c3a14115760a1d2dd355dc95be43b149
DaiYu <122267888@qq.com>
2015-12-16 11:24:02 +0800

    Update 019_RemoveNthNodeFromEnd.java

--- Java/019_RemoveNthNodeFromEnd.java
+++ Java/019_RemoveNthNodeFromEnd.java
@@ -48,3 +48,33 @@ public class Solution {
         }
     }
 }
+//1ms
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode p = dummy;
+        ListNode q = dummy;
+        
+        for (int i = 0; i < n; i ++) {
+            p = p.next;
+        }
+        
+        while (p.next != null) {
+            p = p.next;
+            q = q.next;
+        }
+        
+        q.next = q.next.next;
+        
+        return dummy.next;
+    }
+}
